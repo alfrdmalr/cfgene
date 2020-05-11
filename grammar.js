2e49836c435be49cb449f0a93528b153fdea88d9
alfrdmalr <alfrdmalr@gmail.com>
2020-05-11 14:08:53 -0400

    create Grammar object and basic methods

---
+++ grammar.js
@@ -0,0 +1,52 @@
+// grammar.js
+
+class	Grammar {
+	// optional args:
+	// - grammar: JSON object holding production rules for the grammar
+	// - adornSymbols: function to transform a string into a grammar-compliant symbol 
+	constructor(grammar, adornSymbol) {
+		this.grammar = grammar || {};
+		this.adornSymbol = adornSymbol;
+	}
+
+	symbolify(c) {
+		if (this.adornSymbol) {
+			return this.adornSymbol(c);
+		}	else {
+			return `<${c}>`;
+		} 
+	}
+	
+	hasRule(str) {
+		return this.grammar.hasOwnProperty(symbol);
+	}
+
+	getRule(symbol) {
+		return this.grammar[symbol];
+	}
+
+	addRule(symbol, expansion) {
+		if (this.hasRule(symbol)) {
+			throw new Error(`Symbol ${symbol} already has the following rule:\n${this.getRule(symbol)}`);
+		}
+		
+		this.grammar[adornedSymbol] = expansion;
+	}
+
+	updateRule(symbol, expansion) {
+		if (!this.hasRule(symbol)) {
+			throw new Error(`No symbol '${symbol}' found in grammar to update.`);
+		} 
+
+		this.grammar[symbol] = expansion;
+	}
+
+	// removes both the symbol and expansion from the grammar
+	removeRule(symbol) {
+		if (!this.hasRule(symbol)) {
+			throw new Error(`No symbol ${symbol} found in grammar to remove. Success?`);
+		}
+
+		delete this.grammar[symbol];
+	}
+}
